3aef9c834a770bb025a6094ad4bdeea7af2391fe
Y-Esmeijer <507685@student.fontys.nl>
2023-09-20 12:59:53 +0200

    post-test-1

--- Excersize2.py
+++ Excersize2.py
@@ -1,3 +1,4 @@
+
 import numpy as np
 def calculate_coördinates(x_range: type(list), y_range : type(list), width : type(int)):
     """hierbij gebruiken we de range van de waarde om x aantal waardes te leggen op de assen en een 2d numpy array te maken met de bijbehorende coordinaten"""
@@ -21,13 +22,13 @@ def calculate_value(x: type(int),y: type(int)):
 
     while n != 0:
         if n == 100:
-            return n = 0
+            n = 0
         if abs(a_n) > 2:
-            return n
+            end
         
         a_n = a_n**2 + complex (x, y)
         n += 1
-
+    return n
 
     #while loop, als n == 100 -> n = 0, abs momenteel nummer > 2 geef n. anders n + 1 & functie a_n
 
@@ -43,10 +44,10 @@ def combine_values(coordinates_graph):
 
 def assign_color(all_values):
     """hierbij wordt de diverging index omgezet naar een value op het kleurenschema"""
-     # maak waarde 0 tot ? naar 0 tot 255 voor kleur 
-     max_n = all_values.max()
-     color_values = np.array()
-     for i in all_values:
+    # maak waarde 0 tot ? naar 0 tot 255 voor kleur 
+    max_n = all_values.max()
+    color_values = np.array()
+    for i in all_values:
         color_row = np.array()
         for j in i:
             color = (j*255)/max_n
