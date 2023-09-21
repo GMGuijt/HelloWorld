01676d4adc69f8ea25c0b107c5fb0b783eea082f
BasvBeem <b.vanbeem@student.fontys.nl>
2023-09-21 17:25:19 +0200

    Exersize3Bas gemaakt en gedaan shit yk

--- Exersize3Bas.py
+++ Exersize3Bas.py
@@ -2,6 +2,21 @@ import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.stats as sp
+import random
+
+TotalNodes = 50
+
+def main(TotalNodes):
+    """Main function foor creating the network"""
+    Network = CreateNetwork()
+    for i in range(5,TotalNodes):
+        Addnode(i,Network)
+        print(i)
+    print('start fillup')
+    FillUp(Network)
+    nx.draw(Network,node_size = 5)
+    plt.show()
+
 def CreateNetwork():
     """Creates a network with a total of five nodes"""
     Network = nx.star_graph(4)
@@ -12,12 +27,14 @@ def CountEdges(Network,node):
     """Counts the amount of edges a node has"""
     count = 0
     for node2 in range(len(Network)):
+        a = nx.number_of_nodes(Network)
         count += Network.number_of_edges(node,node2)
     return count
 
 def AddEdges(Network,node):
     """Add edges to a node"""
-    for i in range(len(Network)):
+    #Forloop for calculating pi and plotting the edge
+    for i in range(len(Network)-1):
         if CountEdges(Network,i) == 4:
             continue
         di = CountEdges(Network,i)
@@ -29,18 +46,26 @@ def AddEdges(Network,node):
             Network.add_edge(node,i)
         if CountEdges(Network,node) == 4:
             break
-    print(pi)
     return Network
-    
+
+def FillUp(Network):
+    count = 0
+    nodes = range(TotalNodes)
+    while nx.number_of_edges(Network) < 2*TotalNodes:
+        node,node1 = nodes(random.randint(0,len(nodes))),nodes(random.randint(0,len(nodes)))
+        if node == node1:
+            continue
+        edges_node = CountEdges(Network,node)
+        edges_node1 = CountEdges(Network,node1)
+        if edges_node<4 and edges_node1<4:
+            Network.add_edge(node,node1)
+            print('added note')
+        elif edges_node1 == 4:
+            nodes.pop()
+        count += 1
+
 def Addnode(node,Network):
     """Creates a new node and adds al neccecary edges"""
     Network.add_node(node)
     AddEdges(Network,node)
-
-def main():
-    Network = CreateNetwork()
-    for i in range(5,50):
-        Addnode(i,Network)
-    nx.draw(Network,node_size = 5)
-    plt.show()
-main()
+main(TotalNodes)
--- test.py
+++ test.py
@@ -1,8 +1,4 @@
 import matplotlib.pyplot as plt
 import random
 
-a = [random.random,random.random,random.random,random.random,random.random,random.random]
-b = [random.random,random.random,random.random,random.random,random.random,random.random]
-
-plt.plot(a,b)
-plt.show()
+random(nodes)
