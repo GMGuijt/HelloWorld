daa433017914da4c47d67545b365c07e64d00494
Y-Esmeijer <507685@student.fontys.nl>
2023-09-24 20:03:01 +0200

    cleaned up if statement

--- Excersize2.py
+++ Excersize2.py
@@ -1,12 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 def calculate_coördinates_values(x_range, y_range, width):
-    """hierbij gebruiken we de range van de waarde om x aantal waardes te leggen op de assen en een 2d numpy array te maken met de bijbehorende coordinaten"""
+    """hierbij gebruiken we de range van de waarde om x aantal waardes te leggen op de assen en een 3d numpy array te maken met de bijbehorende coordinaten"""
     value_array = np.zeros([width, width, 1])
-    for i in range(0, width):
+    for i in range(width):
         y = y_range[0] + i*(y_range[1]-y_range[0])/width #bereken width aantal y coordinaten
         X = np.zeros([width, 1])
-        for j in range(0, width):
+        for j in range(width):
             x = x_range[0] + j*(x_range[1]-x_range[0])/width #bereken voor elk y coordinaat een width aantal x coordinaten
             value = calculate_value(x,y) 
             X[j] = value
@@ -15,20 +15,14 @@ def calculate_coördinates_values(x_range, y_range, width):
 
 def calculate_value(x,y):
     """use this function to calculate an iteration of a mandelbrot, if it diverges to infinity return 0"""
-    n = 1
+    n = 0
     a_n = 0
-    end = False
 
-    while end == False:
-        if n == 100:
-            n = 0
-            end = True
-        if abs(a_n) > 2:
-            end = True
-        
+    while n!= 100 and abs(a_n) <= 2 :
         a_n = a_n**2 + complex (x, y)
         n += 1
-    return n
+
+    return n % 100
 
 def plot_image(array_color_values):
     plt.imshow(array_color_values)
