4da53680acc12fad883264079c840969cbc3df07
GMGuijt <508035@student.fontys.nl>
2023-09-29 15:10:05 +0200

    opdr 1

--- week4/Excersize1.py
+++ week4/Excersize1.py
@@ -1,5 +1,20 @@
 import pandas as pd
 
 
-df = pd.read_excel('week4\dataProject4.xlsx', sheet_name='20000-211000', header = 4)
-print(df)
+df = pd.read_excel('week4\dataProject4.xlsx',1, header = 4)
+
+#print(df.loc[:,'Postal Code':'1c. Brand name'])
+
+def remove_empty(df):
+    data_only = df[df.loc[:,'HL':'HL.4'].sum(axis=1) == 0]
+    for i in data_only.index:
+        df.drop(i)
+    ndf = df
+    return ndf
+
+
+
+print(len(df))
+ndf = remove_empty(df)
+print(len(ndf))
+
