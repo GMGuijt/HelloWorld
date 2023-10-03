090e533066c5d6b2c157d04002c043170d807746
Y-Esmeijer <507685@student.fontys.nl>
2023-10-03 13:53:58 +0200

    4.1 setup

---
+++ week4/Excercize4.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+#$conda install -c conda-forge textblob
+#$python -m textblob.download_corpora
+
+#import textblob as tb
+
+
+
+df = pd.read_excel('tweets.xlsx')
+
+
+def list_singular_entity(dataframe, column_name):
+    enitities = []
+    for i in range(dataframe):
+        enitities.append(dataframe[column_name])
+    return enitities
+        
+
+
+def detect_languages(dataframe, column_name):
+    entities = list_singular_entity(dataframe, column_name)
+    entity_language_dict = {}
+    for i in entities:
+        language = tb.TextBlob(i)
+        entity_language_dict.append(language, i)
+    return entity_language_dict
+
+detect_languages(df, 'Tweet')
+    
+
+
