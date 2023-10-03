ee283ccb57097a70744eca984e60a61a919c9d36
GMGuijt <508035@student.fontys.nl>
2023-10-03 15:14:44 +0200

    progressie, met de bedachte data cleaning methods genoteerd

--- week4/Excersize1.py
+++ week4/Excersize1.py
@@ -5,16 +5,28 @@ df = pd.read_excel('week4\dataProject4.xlsx',1, header = 4)
 
 #print(df.loc[:,'Postal Code':'1c. Brand name'])
 
-def remove_empty(df):
-    data_only = df[df.loc[:,'HL':'HL.4'].sum(axis=1) == 0]
+def remove_empty(data):
+    '''verwijder rijen waarvoor de totale inkoopheoveelheid 0 is'''
+    ndf = df.copy
+    data_only = data[data.loc[:,'HL':'HL.4'].sum(axis=1) == 0]
     for i in data_only.index:
-        df.drop(i)
-    ndf = df
+        ndf.drop(i)
     return ndf
 
+def remove_dupes(data):
+    '''verwijder duplicate rijen'''
+    for i in range(len(data)):
+        for j in range(i+1,len(data)):
+            if data[i].loc[:,'','HL.4']
+
+def collapse_doubles(data):
+    '''sommeer producten die tweemaal of vaker door dezelfde klant is gekocht'''
 
 
-print(len(df))
-ndf = remove_empty(df)
-print(len(ndf))
+#def 
+
+#ndf = remove_empty(df)
+#print(len(df))
+#print(len(ndf))
+remove_dupes(range(6))
 
