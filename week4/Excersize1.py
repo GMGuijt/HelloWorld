3d3f3f44e9bf1f2db051608f776bca91b910153c
GMGuijt <508035@student.fontys.nl>
2023-10-09 10:50:42 +0200

    exercise 1 done

--- week4/Excersize1.py
+++ week4/Excersize1.py
@@ -1,47 +1,26 @@
 import pandas as pd
 
-
-df = pd.read_excel('week4\dataProject4.xlsx',1, header = 4)
-
-#print(df.loc[:,'Postal Code':'1c. Brand name'])
-
-<<<<<<< HEAD
-def remove_empty(df):
-    ndf = df.copy()
-    data_only = df[df.loc[:,'HL':'HL.4'].sum(axis=1) == 0]
-    for i in data_only.index:
-        df.drop(i)
-=======
 def remove_empty(data):
     '''verwijder rijen waarvoor de totale inkoopheoveelheid 0 is'''
     ndf = df.copy
     data_only = data[data.loc[:,'HL':'HL.4'].sum(axis=1) == 0]
     for i in data_only.index:
         ndf.drop(i)
->>>>>>> d39967629f80c11f60f84e410b0655cf7d4fc8c4
     return ndf
 
 def remove_dupes(data):
     '''verwijder duplicate rijen'''
-    remove = []
-    for i in range(len(data)):
-        for j in range(i+1,len(data)):
-            if list(data.loc[i]) == list(data.loc[j]):
-                remove.append(i)
-            else:
-                continue
-    print(remove)
+    data.drop_duplicates(keep='first',inplace=True)
+    return data
 
 def collapse_doubles(data):
     '''sommeer producten die tweemaal of vaker door dezelfde klant is gekocht'''
+    ndf = data.groupby(['2f. Customer number','Postal Code	Customer Classification (CRM)','Horeca menu webshop','Prd. name Webshop	1c.','Brand name','Contents']).sum().reset_index()
+    return ndf
 
-<<<<<<< HEAD
-re
-print(len(df))
-print(len(ndf))
-=======
- 
-
-remove_dupes(df)
-
->>>>>>> d39967629f80c11f60f84e410b0655cf7d4fc8c4
+def main(df):
+    df = pd.read_excel('week4\dataProject4.xlsx',1, header = 4)
+    df = remove_empty(df)
+    df = remove_dupes(df)
+    df = collapse_doubles(df)
+    return df
